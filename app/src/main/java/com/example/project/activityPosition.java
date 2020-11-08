e1cac835bd508eb38e6aaf702982443049c6a512
tal zaidman <Talzaidman5@gmail.com>
2020-11-08 17:43:26 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/project/NotYourProject.java

--- app/src/main/java/com/example/project/activityPosition.java
+++ app/src/main/java/com/example/project/activityPosition.java
@@ -1,11 +1,24 @@
 package com.example.project;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project.R;
 
 public class activityPosition extends AppCompatActivity {
+
+    private Button click_me;
+    private TextView text;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/project/fetchData.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+import android.os.AsyncTask;
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+     //   URL url = new URL()
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
