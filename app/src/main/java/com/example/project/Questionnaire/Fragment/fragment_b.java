a09b728810c01de5b497fd2e3fe79da0735f80ef
or_zaidman <orz@playtium.com>
2021-04-02 15:05:59 +0300

    remove dateformat

--- app/src/main/java/com/example/project/Questionnaire/Fragment/fragment_b.java
+++ app/src/main/java/com/example/project/Questionnaire/Fragment/fragment_b.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,15 +17,15 @@ import com.example.project.R;
 
 public class fragment_b extends Fragment {
     private View view;
-    private TextView fragment_b_TXT;
+    private TextView fragment_b_TXT,test;
     private Button next;
-
+    private CheckBox one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if(view == null)
-            view = inflater.inflate(R.layout.freagment_b,container,false);
+        if (view == null)
+            view = inflater.inflate(R.layout.freagment_b, container, false);
 
         findViews(view);
         next = view.findViewById(R.id.next);
@@ -32,7 +33,7 @@ public class fragment_b extends Fragment {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //   if(checkData()){
+//                   if(checkData()){
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 Fragment fragment = new fragment_c();
                 ft.replace(R.id.main_LIN, fragment);
@@ -44,11 +45,37 @@ public class fragment_b extends Fragment {
         return view;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+
     private void findViews(View view) {
-        //fragment_b_TXT = view.findViewById(R.id.fragment_b_TXT);
+        one = view.findViewById(R.id.one);
+        two = view.findViewById(R.id.two);
+        three = view.findViewById(R.id.three);
+        four = view.findViewById(R.id.four);
+        five = view.findViewById(R.id.five);
+        six = view.findViewById(R.id.six);
+        seven = view.findViewById(R.id.seven);
+        eight = view.findViewById(R.id.eight);
+        nine = view.findViewById(R.id.nine);
+        ten = view.findViewById(R.id.ten);
+        seven = view.findViewById(R.id.eleven);
+        twelve = view.findViewById(R.id.twelve);
+        test = view.findViewById(R.id.test);
     }
 
 
+    private boolean checkData() {
+        if (one.isChecked() || two.isChecked() || three.isChecked() || four.isChecked() ||
+                five.isChecked() || six.isChecked() || seven.isChecked() || eight.isChecked() ||
+                nine.isChecked() || ten.isChecked() || eleven.isChecked() || twelve.isChecked())
+            return false;
+        return true;
+    }
 
 
 }
+
