76074a32e52a63a15c66fe01ad9e8e6e47ecd741
or_zaidman <orz@playtium.com>
2021-05-07 10:45:44 +0300

    add missing data

--- app/src/main/java/com/example/project/activitis/client/ActivityMyProfile.java
+++ app/src/main/java/com/example/project/activitis/client/ActivityMyProfile.java
@@ -279,6 +279,13 @@ public class ActivityMyProfile extends AppCompatActivity {
         add_blood_donation_TXT_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(ActivityMyProfile.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener2, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
         });
         mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
